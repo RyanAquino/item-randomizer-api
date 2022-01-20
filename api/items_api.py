b17b4f86207eca296f8b4024d4fcf0771643cdbb
RyanAquino <ryan.aquino7110@gmail.com>
2022-01-20 21:37:40 +0800

    IRA-3: Added requirements.txt and GET download item endpoint

--- api/items_api.py
+++ api/items_api.py
@@ -6,6 +6,7 @@ import string
 from random import choice, randint, uniform
 from typing import Optional
 
+from flask import send_file
 from flask_restful import Resource
 
 
@@ -19,6 +20,10 @@ class ItemResource(Resource):
         :param item_id: Item id
         :return: Item object details
         """
+        # Count alphabet strings
+        # Count real numbers
+        # Count integers
+        # Count alphanumeric strings
         return {"get": f"/v1/items/{item_id}"}
 
 
@@ -26,13 +31,25 @@ class DownloadableItemResource(Resource):
     """
     Downloadable file Item API Resource
     """
-    def get(self, item_id: int):
+
+    def get(self):
         """
         Retrieve Item object file
-        :param item_id: Item id
         :return: Item object file
         """
-        return {"get": f"/v1/items/{item_id}/download"}
+        file_name = "output.txt"
+        file_path = f"../{file_name}"
+
+        if not os.path.exists(file_name):
+            return {"detail": "File not found"}, 404
+
+        response = send_file(
+            path_or_file=file_path,
+            mimetype="application/octet-stream",
+            as_attachment=True,
+            attachment_filename=file_name
+        )
+        return response
 
 
 class ItemsResource(Resource):
@@ -80,10 +97,9 @@ class ItemsResource(Resource):
         file_name = "output.txt"
         file_size_buffer = 10000
         max_file_size = ((1024 * 1024) * 2) - file_size_buffer  # 2MB
-        open(file_name, "w")
-        file_size = os.stat(file_name).st_size
 
         with open(file_name, 'w') as f:
+            file_size = os.stat(file_name).st_size
             f.truncate()
             while file_size <= max_file_size:
                 func_list = [
@@ -96,5 +112,4 @@ class ItemsResource(Resource):
                 f.write(f"{output}, ")
                 file_size = os.stat(file_name).st_size
 
-            f.close()
         return {"file_name": file_name}, 201
--- api/routes.py
+++ api/routes.py
@@ -5,5 +5,5 @@ from api import api
 from api.items_api import ItemResource, DownloadableItemResource, ItemsResource
 
 api.add_resource(ItemsResource, "/v1/items")
-api.add_resource(ItemResource, "/v1/items/<item_id>")
-api.add_resource(DownloadableItemResource, "/v1/items/<item_id>/download")
+api.add_resource(ItemResource, "/v1/items/<int:item_id>")
+api.add_resource(DownloadableItemResource, "/v1/item/download")
