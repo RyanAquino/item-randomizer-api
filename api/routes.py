749b73de33a54d59f3e6d6b1bfcb7956e8cffbe8
RyanAquino <ryan.aquino7110@gmail.com>
2022-01-22 23:35:07 +0800

    IRA-10: Create swagger API documentation

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,18 @@
 """
 API Routes
 """
-from api import api
+from flask_swagger_ui import get_swaggerui_blueprint
+
+from api import api, app
 from api.items_api import DownloadableItemResource, ItemsResource
+
+
+SWAGGER_URL = "/api-docs"
+API_URL = "/static/swagger.yaml"
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Item Randomizer API"}
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 api.add_resource(ItemsResource, "/v1/item")
 api.add_resource(DownloadableItemResource, "/v1/item/download")
